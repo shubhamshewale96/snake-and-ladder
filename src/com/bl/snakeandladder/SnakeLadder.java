c6cf1fcd4afb5c5540c8df16c7594e4e989a191d
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-09-28 18:38:24 +0530

    Uc2 The player rolls a die to get the number between 1 to 6

--- src/com/bl/snakeandladder/SnakeLadder.java
+++ src/com/bl/snakeandladder/SnakeLadder.java
@@ -1,9 +1,23 @@
 package com.bl.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println(" Welcome to Snake And Ladder ");
 		int player1 = 0;// start position 0 uc1
+		SnakeLadder sn = new SnakeLadder();
+		sn.throwingDice();
 	}
+	
+	public int throwingDice( ) {
+		Random rand = new Random();
+		int upperbound = 7;
+		int lowerbound = 1;
+		int randomDice = rand.nextInt(upperbound-lowerbound) + lowerbound;
+		System.out.println("Random dice number:" + randomDice);
+		return randomDice;
+	}
+
 }
