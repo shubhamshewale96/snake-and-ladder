a53bccb9c463e341f7d269629bace85e91e310c1
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-09-28 18:16:06 +0530

    Snake and Ladder

---
+++ src/com/bl/snakeandladder/SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.bl.snakeandladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println(" Welcome to Snake And Ladder ");
+		int player1 = 0;// start position 0 uc1
+	}
+}
